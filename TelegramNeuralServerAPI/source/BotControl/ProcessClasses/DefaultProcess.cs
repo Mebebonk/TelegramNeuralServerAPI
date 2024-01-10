cbd3bf377a4f801d43d0d0ed70fd6f4a05d526e1
Arkadiy Zakharov <69598137+Mebebonk@users.noreply.github.com>
2024-01-10 12:17:55 +0500

    added person class and draw

--- TelegramNeuralServerAPI/source/BotControl/ProcessClasses/DefaultProcess.cs
+++ TelegramNeuralServerAPI/source/BotControl/ProcessClasses/DefaultProcess.cs
@@ -2,11 +2,134 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TelegramNeuralServerAPI
 {
-	internal class Person
+	internal class Person(FaceDetector faceDetector, Fitter? fitter, AgeEstimator? ageEstimator, LivenessEstimator? livenessEstimator, GenderEstimator? genderEstimator, EmotionEstimator? emotionEstimator, MaskEstimator? maskEstimator, GlassesEstimator? glassesEstimator)
 	{
+
+		[JsonInclude]
+		[JsonPropertyName("boundingBox")]
+		public FaceDetector faceDetector = faceDetector;
+
+		[JsonInclude]
+		[JsonPropertyName("FITTER")]
+		public Fitter? fitter = fitter;
+
+		[JsonInclude]
+		[JsonPropertyName("AGE_ESTIMATOR")]
+		public AgeEstimator? ageEstimator = ageEstimator;
+
+		[JsonInclude]
+		[JsonPropertyName("LIVENESS_ESTIMATOR")]
+		public LivenessEstimator? livenessEstimator = livenessEstimator;
+
+		[JsonInclude]
+		[JsonPropertyName("GENDER_ESTIMATOR")]
+		public GenderEstimator? genderEstimator = genderEstimator;
+
+		[JsonInclude]
+		[JsonPropertyName("EMOTION_ESTIMATOR")]
+		public EmotionEstimator? emotionEstimator = emotionEstimator;
+
+		[JsonInclude]
+		[JsonPropertyName("MASK_ESTIMATOR")]
+		public MaskEstimator? maskEstimator = maskEstimator;
+
+		[JsonInclude]
+		[JsonPropertyName("GLASSES_ESTIMATOR")]
+		public GlassesEstimator? glassesEstimator = glassesEstimator;
+	}
+
+	[method: JsonConstructor]
+	internal struct Coordinate(int x, int y)
+	{
+		[JsonInclude]
+		[JsonPropertyName("x")]
+		public int x = x;
+		[JsonInclude]
+		[JsonPropertyName("y")]
+		public int y = y;
+	}
+
+	[method: JsonConstructor]
+	internal readonly struct FaceDetector(Coordinate topLeft, Coordinate bottomRight)
+	{
+		[JsonInclude]
+		[JsonPropertyName("topLeft")]
+		public readonly Coordinate topLeft = topLeft;
+		[JsonInclude]
+		[JsonPropertyName("bottomRight")]
+		public readonly Coordinate bottomRight = bottomRight;
+	}
+
+	[method: JsonConstructor]
+	internal readonly struct Fitter(Coordinate leftEye, Coordinate rightEye, Coordinate mouth, Coordinate[] keypoints)
+	{
+		[JsonInclude][JsonPropertyName("left_eye")] public readonly Coordinate leftEye = leftEye;
+		[JsonInclude][JsonPropertyName("right_eye")] public readonly Coordinate rightEye = rightEye;
+		[JsonInclude][JsonPropertyName("mouth")] public readonly Coordinate mouth = mouth;
+
+		[JsonInclude][JsonPropertyName("keypoints")] public readonly Coordinate[] keypoints = keypoints;
+	}
+
+	[method: JsonConstructor]
+	internal readonly struct EmotionEstimator(float angry, float disgusted, float scared, float happy, float neutral, float sad, float surprised)
+	{
+		[JsonInclude][JsonPropertyName("ANGRY")] public readonly float angry = angry;
+		[JsonInclude][JsonPropertyName("DISGUSTED")] public readonly float disgusted = disgusted;
+		[JsonInclude][JsonPropertyName("SCARED")] public readonly float scared = scared;
+		[JsonInclude][JsonPropertyName("HAPPY")] public readonly float happy = happy;
+		[JsonInclude][JsonPropertyName("NEUTRAL")] public readonly float neutral = neutral;
+		[JsonInclude][JsonPropertyName("SAD")] public readonly float sad = sad;
+		[JsonInclude][JsonPropertyName("SURPRISED")] public readonly float surprised = surprised;
+	}
+
+	[method: JsonConstructor]
+	internal readonly struct AgeEstimator(int age)
+	{
+		[JsonInclude]
+		[JsonPropertyName("age")]
+		public readonly int age = age;
+	}
+
+	[method: JsonConstructor]
+	internal readonly struct LivenessEstimator(float confidence, string value)
+	{
+		[JsonInclude]
+		[JsonPropertyName("confidence")]
+		public readonly float confidence = confidence;
+		[JsonInclude]
+		[JsonPropertyName("value")]
+		public readonly string value = value;
+	}
+
+	[method: JsonConstructor]
+	internal readonly struct GenderEstimator(string gender)
+	{
+		[JsonInclude]
+		[JsonPropertyName("gender")]
+		public readonly string gender = gender;
+	}
+
+	[method: JsonConstructor]
+	internal readonly struct MaskEstimator(bool hasMask)
+	{
+		[JsonInclude]
+		[JsonPropertyName("hasMask")]
+		public readonly bool hasMask = hasMask;
+	}
+
+	[method: JsonConstructor]
+	internal readonly struct GlassesEstimator(bool hasGlasses, float confidence)
+	{
+		[JsonInclude]
+		[JsonPropertyName("hasGlasses")]
+		public readonly bool hasGlasses = hasGlasses;
+		[JsonInclude]
+		[JsonPropertyName("confidence")]
+		public readonly float confidence = confidence;
 	}
 }
--- TelegramNeuralServerAPI/source/botControl/LocalBotUpdate.cs
+++ TelegramNeuralServerAPI/source/botControl/LocalBotUpdate.cs
@@ -12,12 +12,13 @@ using System.Text.Json.Serialization;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using Emgu.CV.Features2D;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TelegramNeuralServerAPI
 {
 	internal class LocalBotUpdate(ITelegramBotClient botClient, Update update, Task<LocalUserConfig> userCfg, HttpRequestHandler requestHandler, CancellationToken cancellationToken)
 	{
-
+		static readonly JsonSerializerOptions personOptions = new() { IncludeFields = true };
 		public async Task RealiseMessage()
 		{
 			try
@@ -80,27 +81,7 @@ namespace TelegramNeuralServerAPI
 					JsonElement.ArrayEnumerator array = JsonDocument.Parse(response).RootElement.GetProperty("result").EnumerateArray();
 					if (array.Count() != images.Count) { throw new("count missmatch"); }
 
-					int i = 0;
-					foreach (JsonElement imageData in array)
-					{
-						JsonElement data = imageData.GetProperty("data");
-						var peopleData = data.EnumerateArray();
-						if (!peopleData.Any()) { throw new("no data found!"); }
-
-						foreach (JsonElement personData in peopleData)
-						{
-							Person person = personData.Deserialize<Person>() ?? throw new("how?..");
-
-						}
-
-						i++;
-					}
-
-					//{
-					//	using FileStream file = new("log.txt", FileMode.Create);
-					//	using StreamWriter writer = new(file);
-					//	writer.Write(response);
-					//}
+					DrawProcessInfo(images, array);
 
 					await ThrowImages(user, images);
 
@@ -155,6 +136,42 @@ namespace TelegramNeuralServerAPI
 
 				img.Value.Dispose();
 				imgMs.Dispose();
+			}
+		}
+		private static void DrawProcessInfo(Dictionary<string, Image<Rgb, byte>> images, JsonElement.ArrayEnumerator array)
+		{
+			int i = 0;
+			foreach (JsonElement imageData in array)
+			{
+				int personNumber = 0;
+				JsonElement data = imageData.GetProperty("data");
+				var peopleData = data.EnumerateArray();
+				if (!peopleData.Any()) { throw new("no data found!"); }
+
+				foreach (JsonElement personData in peopleData)
+				{
+					Image<Rgb, byte> currentImage = images.Values.ToArray()[i];
+					Person person = personData.Deserialize<Person>(personOptions) ?? throw new("how?..");
+
+					int width = Math.Abs(person.faceDetector.topLeft.x - person.faceDetector.bottomRight.x);
+					int height = Math.Abs(person.faceDetector.topLeft.y - person.faceDetector.bottomRight.y);
+
+					int thickness = (int)Math.Ceiling(Math.Min(width, height) * 0.01);
+					int borderThickness = (int)Math.Ceiling(thickness * 1.5);
+
+					Rgb personColor = new(System.Drawing.Color.Yellow);
+					Rgb borderColor = new(System.Drawing.Color.Black);
+
+					currentImage.Draw(rect: new(person.faceDetector.topLeft.x, person.faceDetector.topLeft.y, width, height), borderColor, borderThickness);
+					currentImage.Draw(rect: new(person.faceDetector.topLeft.x, person.faceDetector.topLeft.y, width, height), personColor, thickness);
+
+					currentImage.Draw(personNumber.ToString(), new System.Drawing.Point(person.faceDetector.topLeft.x + thickness * 2, person.faceDetector.bottomRight.y - thickness * 2), Emgu.CV.CvEnum.FontFace.HersheySimplex, 1.0, borderColor, borderThickness);
+					currentImage.Draw(personNumber.ToString(), new System.Drawing.Point(person.faceDetector.topLeft.x + thickness * 2, person.faceDetector.bottomRight.y - thickness * 2), Emgu.CV.CvEnum.FontFace.HersheySimplex, 1.0, personColor, thickness);
+
+					personNumber++;
+				}
+
+				i++;
 			}
 		}
 
